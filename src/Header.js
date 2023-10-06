4f8d2a78accc863ca9421ba3b8190da78f3a460e
Roshni Jha <roshnijha21@navgurukul.org>
2023-10-06 16:32:04 +0530

    navbar-completed

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,14 @@ import { AppBar, Tab, Tabs, Toolbar } from '@material-ui/core';
 const Header = () => {
   return (
     <AppBar color="primary">
-     
+      <Toolbar>
+        <Tabs textColor="inherit">
+          <Tab label="Home" />
+          <Tab label="About" />
+          <Tab label="Table" />
+          <Tab label="Table2" />
+        </Tabs>
+      </Toolbar>
     </AppBar>
   );
 };
