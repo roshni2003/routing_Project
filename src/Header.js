afd21025bfffc1945ba801e584dcd41970dd0d93
Roshni Jha <roshnijha21@navgurukul.org>
2023-10-06 16:31:10 +0530

    navbar

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppBar, Tab, Tabs, Toolbar } from '@material-ui/core';
+
+const Header = () => {
+  return (
+    <AppBar color="primary">
+     
+    </AppBar>
+  );
+};
+
+export default Header;
