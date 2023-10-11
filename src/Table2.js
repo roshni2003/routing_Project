f327db03c3d7d9976983763d28dedfc0d8445991
Roshni Jha <roshnijha21@navgurukul.org>
2023-10-11 15:16:01 +0530

    Table using api done

--- src/Table2.js
+++ src/Table2.js
@@ -3,22 +3,22 @@ import React, { useEffect, useState } from 'react';
 const Table2 = () => {
   const [data, setData] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=97498dead7f62c325c2a40627936b914&language=en-US&page=1');
-  //       const result = await response.json();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=97498dead7f62c325c2a40627936b914&language=en-US&page=1');
+        const result = await response.json();
         
-  //       if (result.results) {
-  //         setData(result.results);
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
+        if (result.results) {
+          setData(result.results);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, []);
 
   const headers = ['ID', 'Title', 'Overview'];
 
